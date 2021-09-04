2e90c6702a5dadabb51c48ff87bfa09f68ef25b9
gujo <gujo@itu.dk>
2021-09-04 16:19:03 +0200

    corrected project refference for test library

--- 00/exercises.tests/UnitTest1.cs
+++ 00/exercises.tests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace exercises.tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Main_prints_hello_world()
         {
+		// arrange
+		var writer = new StringWriter();
+		Console.SetOut(writer);
 
+		// act
+		Program.Main(new string[0]);
+		var output = writer.GetStringBuilder().ToString().Trim();
+
+		// assert 
+		Assert.Equal("Hello, World!", output);
         }
     }
 }
--- 00/exercises/Program.cs
+++ 00/exercises/Program.cs
@@ -2,11 +2,11 @@
 
 namespace exercises
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
