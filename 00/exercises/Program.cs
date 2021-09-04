5391b02cac30585056ef6d619c35c4c75a999184
gujo <gujo@itu.dk>
2021-09-04 17:26:13 +0200

    implemented IsLeapYear function

--- 00/exercises/Program.cs
+++ 00/exercises/Program.cs
@@ -17,7 +17,7 @@ namespace exercises
         */
         public bool IsLeapYear(int year)
         {
-            throw new NotImplementedException("Writing tests .."); //test comment
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);  
         }
     }
 }
