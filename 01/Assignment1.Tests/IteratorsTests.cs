7e54d7a14ab251280ec09332991e91356a45970d
gujo <gujo@itu.dk>
2021-09-14 17:55:20 +0200

    tested and implemented Flatten

--- 01/Assignment1.Tests/IteratorsTests.cs
+++ 01/Assignment1.Tests/IteratorsTests.cs
@@ -1,8 +1,22 @@
 using Xunit;
+using System.Collections.Generic;
+using System;
 
 namespace Assignment1.Tests
 {
     public class IteratorsTests
     {
+        [Fact]
+        public void Flatten_Given_123456789_returns_123456789()
+        {
+        IEnumerable<IEnumerable<int>> input;
+        input = new int[][]{new int[]{1, 2, 3}, new int[] {4, 5, 6}, new int[]{7, 8, 9}};
+
+        IEnumerable<int> output = Iterators.Flatten(input);
+        
+        
+        IEnumerable<int> expectedOutput = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Assert.Equal(expectedOutput, output);
+        }
     }
 }
--- 01/Assignment1/Iterators.cs
+++ 01/Assignment1/Iterators.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Assignment1
 {
@@ -7,12 +8,19 @@ namespace Assignment1
     {
         public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
         {
-            throw new NotImplementedException();
+            // cleanest solution using Linq
+            return items.SelectMany(item => item);
+            
+            // alternative method with yield implicit implemented and looping over the outer stream
+            /* foreach(var outer in items)
+                foreach(T item in outer)   
+                    yield return item;
+            */
         }
 
         public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
         {
-            throw new NotImplementedException();
+            return items.Where(predicate.Invoke);
         }
 
         public static bool Even(int i) 
@@ -20,9 +28,16 @@ namespace Assignment1
             return i % 2 == 0;
         }
 
+        // let's try with the LeapYear function from assignment 00 as well
+        public static bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        }
+
         public static void Main(string[] args)
         {
             Predicate<int> even = Even;
+            Predicate<int> isLeapYear = IsLeapYear;
         }
     }
 }
