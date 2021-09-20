9187be3330f68dab9ebd7e20e23a94278ab0ee3b
gujo <gujo@itu.dk>
2021-09-20 14:03:20 +0200

    added properties and overrided toString method - only sceleton for now

--- 02/StudentLibrary/Student.cs
+++ 02/StudentLibrary/Student.cs
@@ -4,5 +4,17 @@ namespace StudentLibrary
 {
     public class Student
     {
+        public int Id {}
+        public string GivenName {}
+        public string Surname {}
+        public Status Status {}
+        public DateTime StartDate {}
+        public DateTime EndDate {}
+        public DateTime GraduationDate {}
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
