5011800b04b30d4f28d779ebeaeff5f1d48d0b14
gujo <gujo@itu.dk>
2021-09-20 14:59:18 +0200

    implemented constraints and toString method for the Student class

--- 02/StudentLibrary/Student.cs
+++ 02/StudentLibrary/Student.cs
@@ -4,17 +4,32 @@ namespace StudentLibrary
 {
     public class Student
     {
-        public int Id {}
-        public string GivenName {}
-        public string Surname {}
-        public Status Status {}
-        public DateTime StartDate {}
-        public DateTime EndDate {}
-        public DateTime GraduationDate {}
+        public int Id { get; init; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public Status Status 
+        {
+            get 
+            {
+                var now = DateTime.Now;
+                if (now >= EndDate)
+                    return Status.Dropout;
+                if (now > GraduationDate)
+                    return Status.Graduated;
+                if (now > StartDate && now < GraduationDate)
+                    return Status.Active;
+                return Status.New;
+            }
+        }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime GraduationDate { get; set; }
 
         public override string ToString()
         {
-            throw new NotImplementedException();
+            return $"{this.GivenName} {this.Surname} with id [{this.Id}], has status {this.Status}";
         }
     }
+
+    public enum Status { New, Active, Dropout, Graduated }
 }
