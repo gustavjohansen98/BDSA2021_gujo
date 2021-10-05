38dd400c61058fece4764c9d7bd2e6cf31bcd63b
gujo <gujo@itu.dk>
2021-10-05 19:22:38 +0200

    done with userrepo and associated tests

--- 04/Assignment4.Entities.Tests/UserRepositoryTests.cs
+++ 04/Assignment4.Entities.Tests/UserRepositoryTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 using Assignment4.Core;
 using static Assignment4.Core.Response;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Assignment4.Entities.Tests
 {
@@ -15,7 +16,8 @@ namespace Assignment4.Entities.Tests
             var _repo = new UserRepository(_context);
             Seed(_context);
 
-            var newUser = new UserCreateDTO {
+            var newUser = new UserCreateDTO
+            {
                 Email = "gujo@itu.dk",
                 Name = "Doesn't matter",
             };
@@ -31,7 +33,8 @@ namespace Assignment4.Entities.Tests
             var _repo = new UserRepository(_context);
             Seed(_context);
 
-            var newUser = new UserCreateDTO {
+            var newUser = new UserCreateDTO
+            {
                 Email = "test@test.com",
                 Name = "test",
             };
@@ -55,6 +58,7 @@ namespace Assignment4.Entities.Tests
             Assert.Equal(Deleted, response);
             Assert.Null(_context.Tasks.Find(1).AssignedTo);
             Assert.Null(_context.Tasks.Find(2).AssignedTo);
+            Assert.Null(_repo.Read(3));
         }
 
         [Fact]
@@ -68,6 +72,69 @@ namespace Assignment4.Entities.Tests
             Assert.Equal(Conflict, response);
             Assert.NotNull(_context.Tasks.Find(1).AssignedTo);
             Assert.NotNull(_context.Tasks.Find(2).AssignedTo);
+        }
+
+        [Fact]
+        public void Given_read_2_returns_paolo_tell_as_UserDTO()
+        {
+            var _repo = new UserRepository(_context);
+            Seed(_context);
+
+            var dto = _repo.Read(2);
+
+            Assert.Equal((2, "Paolo Tell", "paolo@itu.dk"), (dto.Id, dto.Name, dto.Email));
+        }
+
+        [Fact]
+        public void Given_read_0_returns_null()
+        {
+            var _repo = new UserRepository(_context);
+            Seed(_context);
+
+            var dto = _repo.Read(0);
+
+            Assert.Null(dto);
+        }
+
+        [Fact]
+        public void ReadAll_return_ReadOnly_Collection()
+        {
+            var _repo = new UserRepository(_context);
+            Seed(_context);
+
+            var list = _repo.ReadAll();
+
+            var expected = new List<UserDTO>();
+            expected.Add(new UserDTO(1, "Rasmus Lystrøm", "rasmus@itu.dk"));
+            expected.Add(new UserDTO(2, "Paolo Tell", "paolo@itu.dk"));
+            expected.Add(new UserDTO(3, "Gustav Johansen", "gujo@itu.dk"));
+
+            Assert.Equal(expected.AsReadOnly(), list);
+        }
+
+        [Fact]
+        public void ReadAll_without_seed_returns_null()
+        {
+            var _repo = new UserRepository(_context);
+            Assert.Null(_repo.ReadAll());
+        }
+
+        [Fact]
+        public void Update_returns_updated_and_new_name_is_inserted_to_enitity_in_context()
+        {
+            var _repo = new UserRepository(_context);
+            Seed(_context);
+
+            var dto = new UserUpdateDTO {
+                Id = 3,
+                Name = "test",
+                Email = "gujo@itu.dk"
+            };
+
+            var respone = _repo.Update(dto);
+
+            Assert.Equal(Updated, respone);
+            Assert.Equal("test", _repo.Read(3).Name);
         }
 
     }
