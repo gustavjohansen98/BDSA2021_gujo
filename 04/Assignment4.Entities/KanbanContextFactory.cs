56d626019bc3fcd4b72c59a7faa190d64e118b08
gujo <gujo@itu.dk>
2021-10-05 13:37:23 +0200

    connected to running instance of mssql in docker container via migrations. will use in memory db from now on during testing

--- 04/Assignment4.Entities/KanbanContextFactory.cs
+++ 04/Assignment4.Entities/KanbanContextFactory.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Assignment4.Entities
 {
@@ -8,7 +10,18 @@ namespace Assignment4.Entities
     {
         public KanbanContext CreateDbContext(string[] args)
         {
-            throw new System.NotImplementedException();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets<KanbanContextFactory>()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("Kanban");
+
+            var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>()
+                .UseSqlServer(connectionString);
+
+            return new KanbanContext(optionsBuilder.Options);
         }
     }
 }
